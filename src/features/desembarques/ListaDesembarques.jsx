a72d0937022058721257ad78690e9107fae917d4
Jean <tu_email@ejemplo.com>
2026-06-22 17:45:00 -0400

    style(tabla): alineacion de columnas, layout y espaciado

--- src/features/desembarques/ListaDesembarques.jsx
+++ src/features/desembarques/ListaDesembarques.jsx
@@ -136,10 +136,10 @@ function ListaDesembarques() {
               <tr>
                 <th scope="col">Especie</th>
                 <th scope="col">Embarcacion</th>
-                <th scope="col">Fecha</th>
-                <th scope="col">Kilos</th>
-                <th scope="col">Estado</th>
-                <th scope="col">Prioridad</th>
+                <th scope="col" className="col-centro">Fecha</th>
+                <th scope="col" className="col-kilos">Kilos (kg)</th>
+                <th scope="col" className="col-centro">Estado</th>
+                <th scope="col" className="col-centro">Prioridad</th>
               </tr>
             </thead>
             <tbody>
