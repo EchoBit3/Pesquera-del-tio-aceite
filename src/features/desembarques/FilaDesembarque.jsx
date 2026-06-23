c4f2c864289b324e20bdad817f61c2fcd9c0bc0a
Jean <tu_email@ejemplo.com>
2026-06-22 21:35:16 -0400

    fix(desembarques): centrar celda de estado en FilaDesembarque

--- src/features/desembarques/FilaDesembarque.jsx
+++ src/features/desembarques/FilaDesembarque.jsx
@@ -8,7 +8,7 @@ function FilaDesembarque({ lote, esPrioritario, onTogglePrioritario }) {
       <td>{embarcacion}</td>
       <td>{fecha}</td>
       <td className="col-kilos">{Number(kilos).toLocaleString('es-CL')} kg</td>
-      <td>
+      <td className="col-centro">
         <span className={`badge badge--${estado}`}>{estado}</span>
       </td>
       <td className="col-centro">
